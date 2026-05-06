fa2be6e9df30ad216d431927fd224e9d638c6dee
Rory Smail <rsmail@calpoly.edu>
2026-05-06 13:23:02 -0700

    attached poster

--- neetcode.py
+++ neetcode.py
@@ -11,8 +11,17 @@ def encode(strs: List[str]) -> str:
     return res
 
 def decode(s: str) -> List[str]:
-    return 0
+    res = []
+    for i in range(len(s)):
+        j = i
+        while j != '%':
+            continue
+        length = int(s[i:j + 1])
+        res.append(s[j : j + length])
+        i = j + length
+    return res
 
 
 ls = ["Hello","World"]
 encode(ls)
+decode(ls)
