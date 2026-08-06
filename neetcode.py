5d19870acb94e664df194f3172be2cd3cf35e82a
Rory Smail <rsmail@calpoly.edu>
2026-08-05 17:31:57 -0700

    updated my about me section

--- neetcode.py
+++ neetcode.py
@@ -1,27 +1,6 @@
-from typing import List
+def printfunc():
+    ls = [1, 2, 3]
+    for i in range(len(ls) - 1):
+        print(i)
 
-
-
-def encode(strs: List[str]) -> str:
-    res = ""
-    for i in range(len(strs)):
-        res += str((len(strs[i])))
-        res +='%'
-        res += (strs[i])
-    return res
-
-def decode(s: str) -> List[str]:
-    res = []
-    for i in range(len(s)):
-        j = i
-        while j != '%':
-            continue
-        length = int(s[i:j + 1])
-        res.append(s[j : j + length])
-        i = j + length
-    return res
-
-
-ls = ["Hello","World"]
-encode(ls)
-decode(ls)
+printfunc()
